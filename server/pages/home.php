9c2cb4245c5037595d4c29e192380750df02a0e0
Tudor Gabriel <tudordangabriel28@gmail.com>
2024-11-28 17:32:52 +0200

    Added header reusable component, injected header and footer into home

--- server/pages/home.php
+++ server/pages/home.php
@@ -3,6 +3,8 @@
 include '../config.php';
 include '../db_connection.php';
 
+$stylesheet = "home.css";
+
 // Query to fetch events
 $eventQuery = "SELECT 
     e.id AS id, 
@@ -52,19 +54,9 @@ $conn->close();
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home page</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../styles/common.css">
-    <link rel="stylesheet" href="../styles/home.css">
-</head>
-<body>
+
+<?php include_once '../components/header.php'; ?>
+<main>
     <div id="hero-image">
         <h1>Come &<br> Be a part<br> of the family</h1>
     </div>
@@ -119,6 +111,7 @@ $conn->close();
 
     </div>
     
-
+</main>
+<?php include_once '../components/footer.php'; ?>
 </body>
 </html>
