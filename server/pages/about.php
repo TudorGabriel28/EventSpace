f181a4eb340879d88b1535d2dd8956ad33aeb647
Alejandro García Menor <alejandro.garciamenor@usp.ceu.es>
2024-12-12 16:01:55 +0100

    Integration of about us page with the components

--- server/pages/about.php
+++ server/pages/about.php
@@ -1,17 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About Us</title>
-    <link rel="stylesheet" href="../../styles/common.css">
-    <link rel="stylesheet" href="../../styles/about-us.css">
-</head>
+<?php 
+    $stylesheet = "about-us.css";
+?>
 <body>
 
-    <header>
-        <?php include '../components/navbar.php'; ?>
-    </header>
+    <?php include_once '../components/header.php'; ?>
 
     <main class="grid-container">
         <section class="title">
